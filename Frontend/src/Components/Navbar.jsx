8ef28b3881caa0a7c2cd7c40436f430f830214d4
SohaibRaza66 <sohaibraza236@gmail.com>
2025-06-12 16:11:07 +0500

    First Commit

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -19,9 +20,9 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav m-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <Link className="nav-link active" aria-current="page" to={"/home"}>
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
