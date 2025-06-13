7fa5fdb760e7cc3a53814dbab870f55c52f04f1f
SohaibRaza66 <sohaibraza236@gmail.com>
2025-06-13 21:24:05 +0500

    First Commit

--- Frontend/src/Components/ForgotPassword.jsx
+++ Frontend/src/Components/ForgotPassword.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-
+import { FaLock, FaEnvelope } from "react-icons/fa";
 const ForgotPassword = ({ setShowNavbar,setShowHeader,setShowFooter }) => {
     useEffect(() => {
     setShowNavbar(false);
@@ -7,23 +7,29 @@ const ForgotPassword = ({ setShowNavbar,setShowHeader,setShowFooter }) => {
     setShowFooter(false);
   }, [setShowNavbar,setShowFooter,setShowHeader]);
   return (
-    <div className="container">
+    <div className="container-fluid tw:bg-purple-200">
       <div className="col-md-12 tw:h-screen">
-        <div className="col-md-6 m-auto tw:translate-y-[50%]">
-          <form action="" className="w-100 shadow-lg rounded p-5">
+        <div className="col-md-4 m-auto tw:translate-y-[10%]">
+          <form action="" className="w-100 shadow-lg rounded p-5 position-relative bg-white">
+          <div className="img">
+            <img src="forget.png" className="img-fluid w-50 m-auto" alt="" />
+          </div>
             <h2 className="border-bottom w-50 p-2 text-center m-auto">Forgot Passsword</h2>
-            <div className="box tw:w-max m-auto">
-            <div className="mb-3 form-group mt-3">
-                <input type="text" className="border-top-0 border-0 border-bottom tw:outline-0 position-relative data" placeholder="Enter Registered Email" required/>
+            <div className="box w-50 m-auto">
+            <div className="mb-3 form-group mt-3 position-relative">
+                <input type="text" className="border-top-0 border-0 border-bottom tw:outline-0 position-relative data w-100" placeholder="Enter Registered Email" required/>
+                <FaEnvelope className="position-absolute tw:end-0 top-0" />
             </div>
-            <div className="mb-3 form-group mt-3">
-                <input type="text" className="border-top-0 border-0 border-bottom tw:outline-0 position-relative data" placeholder="Enter New Password" required />
+            <div className="mb-3 form-group mt-3 position-relative">
+                <input type="text" className="border-top-0 border-0 border-bottom tw:outline-0 position-relative data w-100" placeholder="Enter New Password" required />
+                <FaLock className="position-absolute tw:end-0 top-0" />
             </div>
-            <div className="mb-3 form-group mt-3">
-                <input type="text" className="border-top-0 border-0 border-bottom tw:outline-0 position-relative data" placeholder="Confirm Password" required/>
+            <div className="mb-3 form-group mt-3 position-relative">
+                <input type="text" className="border-top-0 border-0 border-bottom tw:outline-0 position-relative data w-100" placeholder="Confirm Password" required/>
+                <FaLock className="position-absolute tw:end-0 top-0" />
             </div>
-            <div className="mb-3">
-                <button className="btn btn-dark tw:w-50 rounded-5 m-0"> Update Password </button>
+            <div className="mb-3 m-auto">
+                <button className="btn btn-dark w-100 rounded-5 m-0"> Update Password </button>
             </div>
             </div>
           </form>
